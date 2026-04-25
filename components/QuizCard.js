b7072fd89274457fb101d9d8dc50785ee4551c08
taaku-tech <takuya.takeuchi.qq@gmail.com>
2026-04-26 01:12:57 +0900

    make spotify player compact

--- components/QuizCard.js
+++ components/QuizCard.js
@@ -27,9 +27,9 @@ export default function QuizCard({ song, answered, selectedChoice, onAnswer }) {
         style={{ filter: answered ? "none" : "blur(8px)" }}
       >
         <iframe
-          src={`https://open.spotify.com/embed/track/${song.spotifyTrackId}?utm_source=generator`}
+          src={`https://open.spotify.com/embed/track/${song.spotifyTrackId}?utm_source=generator&theme=0`}
           width="100%"
-          height="152"
+          height="80"
           frameBorder="0"
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
